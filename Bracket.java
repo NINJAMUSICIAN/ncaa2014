d20659ae13ff8e0754ce2bec7df5cc0a1ceca8be
Cyron <cyron.completo@gmail.com>
2014-03-18 09:00:47 -0600

    more framework

--- Bracket.java
+++ Bracket.java
@@ -3,4 +3,19 @@ import java.util.Scanner;
 
 public class Bracket {
 
+	private Team a = new Team();
+	private Team b = new Team();
+
+	public static double probability() {
+		a = teams.next().name();
+		b = a.next();
+
+		for (String b : values) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+
+	}
+	// public static int winCount(boolean winState)
+
 }
