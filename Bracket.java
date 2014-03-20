54adbc14940d1dfa5d5957a93051e6ead7883b6b
josh sparks <j80mar4@gmail.com>
2014-03-20 09:14:15 -0600

    yoohoo

--- Bracket.java
+++ Bracket.java
@@ -6,19 +6,20 @@ public class Bracket {
 
 
 	public static double probability(ArrayList<Team> teams) {
-		Team a = teams.next().name();
-		Team b = teams.next();
+		double pab = 0;
+		for(int i = 0; i < teams.size(); i++){
+		Team a = teams.get(i);
+		Team b = teams.get(i + 1);
 
 		double pa = a.wp();
 		double pb = b.wp();
 
 		double pai = pa - (pa * pb);
 		double pao = pa + pb - (2*pa*pb);
-		double pab = pai/pao;
+		pab = pai/pao;
 
 		System.out.println(pab);
+		}
 		return pab;
-
 	}
-
 }
