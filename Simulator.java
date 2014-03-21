227ab89421ebdc293a10076a476356f0095e8ee8
ccomp <cyron.completo@gmail.com>
2014-03-21 11:21:04 -0600

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -3,8 +3,8 @@ public class Simulator {
 	private static final int SIMULATIONS = 1000;
 
 	public static void main(String[] args) {
-//		Bracket bracket = new Bracket();
-//		bracket.simulate(SIMULATIONS);
-//		bracket.printResults();
+		Bracket bracket = new Bracket();
+		bracket.simulate(SIMULATIONS);
+		bracket.printResults();
 	}
 }
